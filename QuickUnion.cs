86226909bd1418c0a52a0a032109cc81ce3f01e7
bereket fiseha <bereketfiseha123@gmail.com>
2022-04-02 14:03:47 +0300

    union find added

---
+++ QuickUnion.cs
@@ -0,0 +1,35 @@
+public class QuickUnion{
+private int[] id;
+ public QuickUnion(int arrayLength){
+
+id= new int[arrayLength];
+
+for(int i=0;i<arrayLength;i++)
+id[i]=i;
+}
+private int root(int i){
+
+if(id[i] == i)
+         return id[i];
+  else        
+        return root(id[i]);
+}
+
+public bool connected(int p,int q){
+
+return root(p)==root(q);
+
+}
+
+public void union(int p,int q){
+
+rootOFP=root(p);
+rootOFQ=root(q);
+
+id[rootOFP]=rootOFQ;
+
+
+}
+
+
+}
